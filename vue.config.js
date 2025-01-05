8c08afc085d975a222014ba5617b7c190154026e
youyouYoung <youyou.young.mine@gmail.com>
2025-01-05 14:06:10 -0600

    remove coreui

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    client: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    },
+    admin: {
+      entry: 'src/main.js',
+      template: 'public/admin/index.html',
+      filename: 'admin/index.html'
+    }
+  }
+}
